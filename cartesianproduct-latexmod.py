d13bbde827fd6609daed203746c3366c70de9edd
PktHammer <44016363+PktHammer@users.noreply.github.com>
2019-01-27 14:37:25 -0500

    Add files via upload

---
+++ cartesianproduct-latexmod.py
@@ -0,0 +1,33 @@
+#print(len(A)) # Discovery of length
+
+# Temporary Globals because I'm bad
+# Using lists [], make a list of the valid set
+
+def twovarproduct(A,B):
+    storedOutput = ''
+    for x in range(len(A)):
+        for y in range(len(B)):
+            storedOutput += '(' + str(A[x]) + ', ' + str(B[y]) + '), '
+    storedOutput = storedOutput[:-2] # Slice the last character
+    #print(storedOutput)
+    storedOutput = latexFormatArray(storedOutput)
+    storedOutput = latexMathModeify(storedOutput)
+    print('Total: ' + str(len(A) * len(B)) + ';', end= ' ')
+    print(storedOutput)
+    
+
+def latexFormatArray(product):
+    product = '\{' + product + '\}'
+    return product
+
+def latexMathModeify(toBeMathed): # Temporary, will update with exceptions soon
+    toBeMathed = '$' + toBeMathed + '$'
+    return toBeMathed
+
+def main():
+    A = ['w', 'x', 'y', 'z']
+    B = ['a', 'b']
+    twovarproduct(B,B)
+
+if __name__ == "__main__":
+    main()
